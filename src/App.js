941a82875b5696c6c3d8225a9e82a4ad5ef56486
Danny Lin <dannyjabez@gmail.com>
2020-11-07 09:49:19 -0500

    edited from LG laptop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Protfolio Test Thinkpad upload
+          Protfolio LG edited
         </p>
         
       </header>
